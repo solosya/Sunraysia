cde73ee690b9e7e87e3a5f75494aac564b03a733
inspheremayank <insphere.mayank@gmail.com>
2023-05-26 20:35:16 +0530

    fixes

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function beforePageView($data)
+{
+    if ($data['slug'] == 'contact') {
+        Yii::$app->response->redirect('/contact-us');
+        Yii::$app->end();
+    }
+}
+
 function beforePageCache() {
     if (Yii::$app->id == 'app-frontend' && Yii::$app->user->isGuest && Yii::$app->controller->id == 'article') {
         $themeConfig = (new \sdk\frontend\Network())->getThemeConfig();
